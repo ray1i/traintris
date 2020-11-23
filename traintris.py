08abee578637d88bd9efd8fd7f730b33fbc82ced
ray1i <43012140+ray1i@users.noreply.github.com>
2020-11-23 02:30:36 -0500

    movement/rotation collision worksnow

--- traintris.py
+++ traintris.py
@@ -1,14 +1,13 @@
 import os
 import random
-import pygame as pg
+import pygame as pg #PYGAME: TOP LEFT IS 0,0
 import sys
 
 import mino_types as types
 
 bag = ['I', 'O', 'T', 'L', 'J', 'Z', 'S']
 random.shuffle(bag) 
-
-#PYGAME: TOP LEFT IS 0,0
+sprites = {}
 
 class Mino:
     def __init__(self, type): #STARTS AT (20, 4)
@@ -16,21 +15,32 @@ class Mino:
         self.perm = 0
         self.ox = 4
         self.oy = 20
-        self.image = pg.image.load(f'{os.path.dirname(__file__)}/{type}.png').convert()
+        self.image = sprites[type]
         self.newcoords()
 
     def newcoords(self):
         self.x = [self.ox + s for s in types.init_x(self.type, self.perm)]
         self.y = [self.oy - s for s in types.init_y(self.type, self.perm)] #flipped bc of pygame's weird coords
 
-    def move(self, h, v, check_collision = True): #1 is right, -1 is left; 1 is up, -1 is down
-        if not check_collision or not collision((self.copy()).move(h, v, False), Board()):
+    def move(self, h, v): #1 is right, -1 is left; 1 is up, -1 is down
+        tempMino = self.copy()
+        tempMino.ox += h
+        tempMino.oy -= v #flipped bc of pygame's weird coords
+        tempMino.newcoords()
+        if not collision(tempMino, Board()):
             self.ox += h
             self.oy -= v #flipped bc of pygame's weird coords
             self.newcoords()
 
     def rotate(self, dir): #1 is cw, -1 is ccw, 2 is 180
-        if not collision(self.copy().rotate(dir), Board()):
+        tempMino = self.copy()
+        tempMino.perm += dir
+        if tempMino.perm < 0:
+            tempMino.perm += 4
+        elif tempMino.perm > 3:
+            tempMino.perm -= 4
+        tempMino.newcoords()
+        if not collision(tempMino, Board()):
             self.perm += dir
             if self.perm < 0:
                 self.perm += 4
@@ -58,11 +68,14 @@ class Board:
 
 def collision(mino, board):
     for p in range(4):
-        if board.blocks[mino.y[p]][mino.x[p]]:
-            return True
-        elif mino.x[p] > 20 or mino.x[p] < 0:
-            return True
-        elif mino.y[p] < 0:
+        try:
+            if board.blocks[mino.y[p]][mino.x[p]]:
+                return True
+            elif mino.x[p] > 20 or mino.x[p] < 0:
+                return True
+            elif mino.y[p] < 0:
+                return True
+        except IndexError:
             return True
     return False
 
@@ -81,6 +94,8 @@ def main():
     board = Board()
     queue = bag
     ingame = True
+    for i in bag:
+        sprites[i] = pg.image.load(f'{os.path.dirname(__file__)}/{i}.png').convert()
 
     das = 100
     arr = 10
@@ -88,7 +103,7 @@ def main():
     das_start = 0
     arr_start = 0
     sd_start = 0
-    ctrl_q = {'DOWN':False, 'LEFT':False, 'RIGHT':False}
+    ctrl_queue = {'DOWN':False, 'LEFT':False, 'RIGHT':False}
 
     currMino = Mino(queue.pop(0))
     while True:
@@ -110,38 +125,38 @@ def main():
                     ### MOVEMENT ###
                     elif event.key == pg.K_DOWN: #DOWN
                         currMino.move(0, -1)
-                        ctrl_q['DOWN'] = True
+                        ctrl_queue['DOWN'] = True
                     elif event.key == pg.K_LEFT: #LEFT
                         currMino.move(-1, 0)
-                        ctrl_q['LEFT'] = True
+                        ctrl_queue['LEFT'] = True
                         das_start = pg.time.get_ticks()
                     elif event.key == pg.K_RIGHT: #RIGHT
                         currMino.move(1, 0)
-                        ctrl_q['RIGHT'] = True
+                        ctrl_queue['RIGHT'] = True
                         das_start = pg.time.get_ticks()
                 
                 elif event.type == pg.KEYUP:
                     if event.key == pg.K_DOWN: #DOWN
-                        ctrl_q['DOWN'] = False
+                        ctrl_queue['DOWN'] = False
                         sd_start = 0
                     elif event.key == pg.K_LEFT: #LEFT
-                        ctrl_q['LEFT'] = False
+                        ctrl_queue['LEFT'] = False
                         arr_start = 0
                     elif event.key == pg.K_RIGHT: #RIGHT
-                        ctrl_q['RIGHT'] = False
+                        ctrl_queue['RIGHT'] = False
                         arr_start = 0
 
             if arr_start > 0:
                 if pg.time.get_ticks() >= arr_start + arr:
-                    if ctrl_q['LEFT']:
+                    if ctrl_queue['LEFT']:
                         currMino.move(-1, 0)
-                    if ctrl_q['RIGHT']:
+                    if ctrl_queue['RIGHT']:
                         currMino.move(1, 0)
                     arr_start += arr
-            elif ctrl_q['LEFT'] or ctrl_q['RIGHT']:
+            elif ctrl_queue['LEFT'] or ctrl_queue['RIGHT']:
                 if pg.time.get_ticks() >= das_start + das:
                     arr_start = pg.time.get_ticks()
-            if ctrl_q['DOWN']:
+            if ctrl_queue['DOWN']:
                 if pg.time.get_ticks() >= sd_start + sd:
                     currMino.move(0, -1)
                     sd_start += sd
